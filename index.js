134b8d00e2fedf2c4c5b9221f048cec09994a689
Milan318 <milanmandaviya7495@gmail.com>
2025-09-03 09:07:20 +0530

    done

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const indexRouter = require("./router/indexRouter");
 const redirectMiddleware = require("./middleware/redirectMiddleware");
 
 
+
 const cookieParser = require('cookie-parser');
 
-const port = 8089;
+const port = 8081;
 const app = express();
 
 app.set("view engine", "ejs");
--- middleware/redirectMiddleware.js
+++ middleware/redirectMiddleware.js
@@ -2,7 +2,6 @@ const redirectMiddleware = (req, res, next) => {
   if (req.url === "/") {
     return res.redirect("/clientHomepage");
   }
-
   next();
 };
 
